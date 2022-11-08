89e4fc1bba570723895b42e24be4fdee8196e1db
Francisbdi <105929523+Francisbdi@users.noreply.github.com>
2022-11-08 19:29:57 -0300

    Create bd_sqlite.py
    Criação do banco de dados e tabela de login.

---
+++ bd_sqlite.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+banco = sqlite3.connect('principal.bd')
+
+cursor = banco.cursor()
+
+cursor.execute("CREATE TABLE usuarios (id INTEGER, login VARCHAR(15) NOT NULL, senha VARCHAR(15) NOT NULL, PRIMARY KEY (id))")
+banco.commit()
+banco.close()
