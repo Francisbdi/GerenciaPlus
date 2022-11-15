2f4bc710ca250ee23f2fa1ce900e5ad59d3e5d07
Francisbdi <105929523+Francisbdi@users.noreply.github.com>
2022-11-14 21:07:33 -0300

    Criei a tabela de cadastro

--- Criador_banco.py
+++ Criador_banco.py
@@ -7,6 +7,10 @@ banco = sqlite3.connect('principal.db')
 cursor = banco.cursor()
 
 #COAMNDO PARA A CRIAÇÃO DE UMA TABELA
-cursor.execute("CREATE TABLE users (id INTEGER NOT NULL, login VARCHAR(15) NOT NULL, senha VARCHAR(12) NOT NUll, PRIMARY KEY(id))")
+#cursor.execute("CREATE TABLE users (id INTEGER NOT NULL, login VARCHAR(15) NOT NULL, senha VARCHAR(12) NOT NUll, PRIMARY KEY(id))")
+#banco.commit()
+#banco.close()
+
+cursor.execute("CREATE TABLE clientes (id INTEGER, nome VARCHAR(50), endereco VARCHAR(50), telefone VARCHAR(13), tempo VARCHAR(4), estabelecimento VARCHAR(50), tipoestabelecimento VARCHAR(15), usuario VARCHAR(30), senha VARCHAR(30))")
 banco.commit()
 banco.close()
