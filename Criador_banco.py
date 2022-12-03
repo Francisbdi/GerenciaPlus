802e136577a3306905e60f9ab0cf80f22d4a0435
Francisbdi <105929523+Francisbdi@users.noreply.github.com>
2022-12-03 20:43:50 -0300

    CORREÇÃO

--- Criador_banco.py
+++ Criador_banco.py
@@ -19,6 +19,6 @@ cursor = banco.cursor()
 #banco.commit()
 #banco.close()
 
-cursor.execute("INSERT INTO barracas (tipo, preco, status) VALUES ('A1', 600, 'DISPONIVEL')")
+cursor.execute("INSERT INTO barracas (tipo, preco, status) VALUES ('R3', 300, 'DISPONIVEL')")
 banco.commit()
 banco.close()
